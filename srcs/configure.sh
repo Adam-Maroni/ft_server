5e02a2e6d7c7ce1f3cc70b07ac1605c5fbecc12b
Adam Maroni <adamaroni@hotmail.fr>
2021-04-17 10:51:29 +0200

    Ajout des SSL

--- srcs/configure.sh
+++ srcs/configure.sh
@@ -1,6 +1,9 @@
 #Lancement de mysql
 service mysql start
 
+#SSL
+mkdir /etc/nginx/ssl
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/ft_server.crt -keyout /etc/nginx/ssl/ft_server.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=amaroni/CN=ft_server"
 
 #Configuration de NGINX
 cp /tmp/default_nginx_conf /etc/nginx/sites-available/default
