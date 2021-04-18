e13ac31b5a371f9abc8e1ecc722f0a3ba474c677
Adam Maroni <adamaroni@hotmail.fr>
2021-04-18 10:48:50 +0200

    MAJ du script de lancement (supposé prêt pour vérification

--- tests/launch.sh
+++ tests/launch.sh
@@ -1,2 +1,2 @@
-docker build -t ft_servertest .
-docker run --rm -ti -p 8080:80 -p 443:443 -v /home/adam/cursus42/ft_server/srcs:/srcs ft_servertest
+docker build -t ft_server .
+docker run --rm -ti -p 8080:80 -p 443:443 ft_server
